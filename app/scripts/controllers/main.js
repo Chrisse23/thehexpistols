7c9cbcebadaafe76be1cef17b5a412a16d43f008
Chrisse23 <christofferwallenberg@hotmail.com>
2017-09-30 11:57:17 +0200

    Login auth

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the baldBritneyApp
  */
 angular.module('baldBritneyApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl',['$scope', function ($scope) {
+    $scope.authorize = function() {
+      var clientId = 'e2b2a53b1aa34ac2ac1dbee3068a740d';
+      var redirectUri = 'http://localhost:9000';
+      var permissions = 'user-read-currently-playing';
+
+      var authorizeUrl = 'https://accounts.spotify.com/authorize';
+      authorizeUrl = authorizeUrl + '?response_type=token';
+      authorizeUrl = authorizeUrl + '&client_id=' + encodeURIComponent(clientId); 
+      authorizeUrl = authorizeUrl + '&redirect_uri=' + encodeURIComponent(redirectUri); 
+      authorizeUrl = authorizeUrl + '&scope=' + encodeURIComponent(permissions);
+
+      window.location = authorizeUrl;
+    };
+  }]);
