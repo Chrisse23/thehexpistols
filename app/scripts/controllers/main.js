93f7d4190c082321250c70626c0ac82102182cb6
Chrisse23 <christofferwallenberg@hotmail.com>
2017-09-30 09:28:53 +0200

    Set up structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name baldBritneyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the baldBritneyApp
+ */
+angular.module('baldBritneyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
